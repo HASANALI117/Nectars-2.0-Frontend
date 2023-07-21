d37032b07caf3d7f48992cc504a35d111753c43a
Husain Abdulla <hussainalhamad.bh@gmail.com>
2023-07-21 15:49:10 +0300

    made product card

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,13 @@
 import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import ProductCard from "./comonents/product/ProductCard";
 
 export default function App() {
-  return <div>App</div>;
+  return (
+    <ProductCard
+      name="Product Name"
+      desc="This product is a product of many products that are produced here."
+      price="$9.99"
+    />
+  );
 }
--- frontend/src/comonents/product/ProductCard.js
+++ frontend/src/comonents/product/ProductCard.js
@@ -1,5 +1,48 @@
-import React from "react";
+// import Button from "react-bootstrap/Button";
+// import Card from "react-bootstrap/Card";
 
-export default function ProductCard() {
-  return <div>ProductCard</div>;
+// function ProductCard(props) {
+//   return (
+//     <Card style={{ width: "18rem" }} className="productCard" border={"dark"}>
+//       <Card.Img
+//         variant="top"
+//         src="https://source.unsplash.com/random/300x202"
+//       />
+//       <Card.Body>
+//         <Card.Title className="productTitle">{props.name}</Card.Title>
+//         <Card.Text>{props.desc}</Card.Text>
+//         <Card.Subtitle className="cardPrice">{props.price}</Card.Subtitle>
+//         <Button variant="success">Buy Now</Button>
+//       </Card.Body>
+//     </Card>
+//   );
+// }
+
+// export default ProductCard;
+
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+
+function ProductCard(props) {
+  return (
+    <Card style={{ width: "18rem" }} className="productCard" border={"dark"}>
+      <div className="cardImageContainer">
+        <Card.Img
+          variant="top"
+          src="https://source.unsplash.com/random/200x400"
+          className="cardImage"
+        />
+        <div className="cardOverlay">
+          <Card.Title className="productTitle">{props.name}</Card.Title>
+          <Card.Text className="cardDescription">{props.desc}</Card.Text>
+          <Card.Subtitle className="cardPrice">{props.price}</Card.Subtitle>
+          <Button variant="danger" className="cardButton">
+            Buy Now
+          </Button>
+        </div>
+      </div>
+    </Card>
+  );
 }
+
+export default ProductCard;
