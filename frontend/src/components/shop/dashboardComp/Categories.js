44cb0f8b5611923efcd40ec51d1d1bf2cd1785b7
khalid Janahi <khalidjanahi094@gmail.com>
2023-07-24 12:18:10 +0300

    populated the shopowner dashboard

--- frontend/src/components/shop/dashboardComp/Categories.js
+++ frontend/src/components/shop/dashboardComp/Categories.js
@@ -1,10 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Col from "react-bootstrap/Col";
 import Nav from "react-bootstrap/Nav";
 import Row from "react-bootstrap/Row";
 import Tab from "react-bootstrap/Tab";
+import axios from "axios";
 
 export default function Categories() {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    // axios.get("/api/categories").then((response) => {
+    //   setCategories(response.data);
+    // });
+    setCategories([
+      { id: 1, name: "Category 1" },
+      { id: 2, name: "Category 2" },
+      { id: 3, name: "Category 3" },
+    ]);
+  }, []);
+
+  const handleDelete = (id) => {
+    setCategories(categories.filter((category) => category.id !== id));
+  };
+
   return (
     <Tab.Container id="left-tabs-example" defaultActiveKey="first">
       <Row>
@@ -20,8 +38,49 @@ export default function Categories() {
         </Col>
         <Col sm={9}>
           <Tab.Content>
-            <Tab.Pane eventKey="first">Category index</Tab.Pane>
-            <Tab.Pane eventKey="second">add Category form</Tab.Pane>
+            <Tab.Pane eventKey="first">
+              <ul className="list-group">
+                {categories.map((category) => (
+                  <li key={category.id} className="list-group-item d-flex">
+                    <div className="flex-grow-1">{category.name}</div>
+                    <div>
+                      <button
+                        type="button"
+                        className="btn btn-danger btn-sm ml-2"
+                        onClick={() => handleDelete(category.id)}
+                      >
+                        Delete
+                      </button>
+                      <button
+                        type="button"
+                        className="btn btn-primary btn-sm ml-2"
+                      >
+                        Edit
+                      </button>
+                    </div>
+                  </li>
+                ))}
+              </ul>
+            </Tab.Pane>
+            <Tab.Pane eventKey="second">
+              <form>
+                <div className="form-group">
+                  <label htmlFor="name">Name</label>
+                  <input type="text" className="form-control" id="name" />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="description">Description</label>
+                  <textarea
+                    className="form-control"
+                    id="description"
+                    rows="3"
+                  ></textarea>
+                </div>
+                <button type="submit" className="btn btn-primary">
+                  Submit
+                </button>
+              </form>
+            </Tab.Pane>
           </Tab.Content>
         </Col>
       </Row>
--- frontend/src/components/shop/dashboardComp/Users.js
+++ frontend/src/components/shop/dashboardComp/Users.js
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Col from "react-bootstrap/Col";
 import Nav from "react-bootstrap/Nav";
 import Row from "react-bootstrap/Row";
 import Tab from "react-bootstrap/Tab";
 
 export default function Users() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    // axios.get("/api/products").then((response) => {
+    //   setProducts(response.data);
+    // });
+    setUsers([
+      { id: 1, name: "user 1"},
+      { id: 2, name: "user 2"},
+      { id: 3, name: "user 3"},
+    ]);
+  }, []);
+
   return (
     <Tab.Container id="left-tabs-example" defaultActiveKey="first">
       <Row>
@@ -13,15 +26,28 @@ export default function Users() {
             <Nav.Item>
               <Nav.Link eventKey="first">Users list</Nav.Link>
             </Nav.Item>
-            <Nav.Item>
+            {/* <Nav.Item>
               <Nav.Link eventKey="second">Add new Product</Nav.Link>
-            </Nav.Item>
+            </Nav.Item> */}
           </Nav>
         </Col>
         <Col sm={9}>
           <Tab.Content>
-            <Tab.Pane eventKey="first">product index</Tab.Pane>
-            <Tab.Pane eventKey="second">add product form</Tab.Pane>
+            <Tab.Pane eventKey="first">
+            <ul className="list-group">
+                {users.map((user) => (
+                  <li
+                    key={user.id}
+                    className="list-group-item d-flex justify-content-between align-items-center"
+                  >
+                    <div>
+                      <div>{user.name}</div>
+                      </div>
+                  </li>
+                ))}
+              </ul>
+            </Tab.Pane>
+            {/* <Tab.Pane eventKey="second">add product form</Tab.Pane> */}
           </Tab.Content>
         </Col>
       </Row>
